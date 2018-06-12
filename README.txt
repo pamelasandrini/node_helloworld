371c7f98c327acf39f5f63b997624148880c8c76
PAMELA SANDRINI BORSONI <pborsoni@br.ibm.com>
2018-06-12 20:55:18 -0300

    add get books functionality

--- README.txt
+++ README.txt
@@ -7,7 +7,14 @@ Run application as: node app
 The main URL to access the project is:
 http://http://localhost:3200/
 
+
+
 Mongo command utils:
+add records using the node_helloworld\resources\insertBooks.txt file
 acess db: use db_name
 find records: db.users.find()
 find records in json format: db.users.find().pretty()
+
+Commands used to create the Node project
+create package.json file: npm init
+install modules: npm install --save express mongoose body-parser
--- routes/app.js
+++ routes/app.js
@@ -7,4 +7,20 @@ router.get('/', function(req, res){
     res.send('Hello World - Node!');
 });
 
+router.get('/books', function(req, res){
+
+    console.log('getting all books');
+    //could use paramaters lik name, id or empty object to get everything
+    book.find({})
+    .exec(function(err, results){
+        if(err){
+            console.log('error has occured while retrieving books');
+            res.send('error has occured');
+        } else {
+            res.json(results);
+        }
+    });
+
+});
+
 module.exports = router;
